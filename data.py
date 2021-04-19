9b991245be16a60881ed15027a7e246536dbe1d7
Sphephelo <sphe.mlu@gmail.com>
2021-04-19 21:50:19 +0200

    Adding the new file

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Article One',
+            'body' : 'Lorem hlkl hlhlk hhuyg khj lglhb byug hkv bljh jkh ghjv chg ghf tuy ui yp hjk hjgh fgh dg trykg hbl uy',
+            'author' : 'Sphe Mlu',
+            'create_date' : '04-25-2021'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article One',
+            'body' : 'Lorem hlkl hlhlk hhuyg khj lglhb byug hkv bljh jkh ghjv chg ghf tuy ui yp hjk hjgh fgh dg trykg hbl uy',
+            'author' : 'zanele Mlu',
+            'create_date' : '04-15-2021'
+        },
+        {
+            'id' : 3,
+            'title' : 'Article One',
+            'body' : 'Lorem hlkl hlhlk hhuyg khj lglhb byug hkv bljh jkh ghjv chg ghf tuy ui yp hjk hjgh fgh dg trykg hbl uy',
+            'author' : 'njabulo Mlu',
+            'create_date' : '04-28-2020'
+        }
+    ]
+    return articles
